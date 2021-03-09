6d3a806545ec27f437743636d6731c0c5f692a67
Ogola Eddy <eddogola01@gmail.com>
2021-03-09 14:53:10 +0300

    added new Client constructor and methods to authorize client privately

--- auth.go
+++ auth.go
@@ -1,5 +1,15 @@
 package unsplash
 
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
 // Permissions scopes
 // To write data on behalf of a user or to access their private data,
 // you must request additional permission scopes from them.
@@ -17,6 +27,117 @@ const (
 
 // Private authorization endpoints
 const (
-	authEndpoint = "https://unsplash.com/oauth/authorize"
+	authCodeEndpoint  = "https://unsplash.com/oauth/authorize"
 	authTokenEndpoint = "https://unsplash.com/oauth/token"
 )
+
+// AuthScopes lists all scopes used in a particular request
+type AuthScopes []string
+
+func (a *AuthScopes) String() string {
+	return strings.Join(*a, "+")
+}
+
+// NewPrivateAuthClient initializes a new client that has been authorised
+// for private actions.
+func NewPrivateAuthClient(ctx context.Context, clientID, clientSecret, redirectURI string, client *http.Client, config *Config, as *AuthScopes) (*Client, error) {
+	if client == nil {
+		client = http.DefaultClient
+	}
+	c := &Client{ClientID: clientID, HTTPClient: client, Config: config}
+	// get authorization code
+	code, err := c.authGetCode(ctx, clientID, redirectURI, as)
+	if err != nil {
+		return nil, err
+	}
+	// get access token
+	authResponse, err := c.authGetAccessToken(ctx, clientID, clientSecret, redirectURI, code)
+	if err != nil {
+		return nil, err
+	}
+	accessToken := authResponse.AccessToken
+	authHeader := fmt.Sprintf("Bearer %s", accessToken)
+	c.Config.Headers.Add("Authentication", authHeader)
+	c.Config.Headers.Add("Accept-Version", "v1") // Add api version
+	// Unsplash strongly encourages a specific request of the api version
+	// do sth to get access token
+	return c, nil
+}
+
+// returns the authorization code used in the subsequent POST request to get access token
+// returns 0 if an error is encountered
+func (c *Client) authGetCode(ctx context.Context, clientID, redirectURI string, as *AuthScopes) (string, error) {
+	queryParams := QueryParams(map[string]string{
+		"client_id":     clientID,
+		"redirect_uri":  redirectURI,
+		"response_type": "code", // The access response type you are requesting. The authorization workflow Unsplash supports requires the value “code” here.
+		"scope":         as.String(),
+	})
+	link, err := buildURL(authCodeEndpoint, queryParams)
+	if err != nil {
+		return "", err
+	}
+	resp, err := c.getHTTP(ctx, link)
+	if err != nil {
+		return "", err
+	}
+
+	// If the user accepts the request, the user will be redirected to the redirect_uri,
+	// with the authorization code in the code query parameter.
+	// get authorization code from the `code` query parameter
+	q := resp.Request.URL.Query()
+	codes, ok := q["code"]
+	if !ok {
+		// `code` query parameter not found, return error
+		return "", errCodeQueryParamNotFound
+	}
+	code := codes[0]
+	if err != nil {
+		return "", err
+	}
+	return code, nil
+}
+
+func (c *Client) authGetAccessToken(ctx context.Context, clientID, clientSecret, redirectURI string, code string) (*AuthResponse, error) {
+	postData := map[string]string{
+		"client_id":     clientID,
+		"client_secret": clientSecret,
+		"redirect_uri":  redirectURI,
+		"code":          code,
+		"grant_type":    "authorization_code",
+	}
+	resp, err := c.postHTTP(ctx, authTokenEndpoint, postData)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	// parse json response
+	var ar AuthResponse
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	err = parseJSON(data, &ar)
+	if err != nil {
+		return nil, err
+	}
+	return &ar, nil
+}
+
+// get a response, by a post request
+func (c *Client) postHTTP(ctx context.Context, link string, postData map[string]string) (*http.Response, error) {
+	data, err := json.Marshal(postData)
+	if err != nil {
+		return nil, err
+	}
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, link, bytes.NewBuffer(data))
+	if err != nil {
+		return nil, err
+	}
+	resp, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
+}
