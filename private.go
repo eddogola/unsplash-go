7b63ac13819b564b77b4eac91a05300ae0fd229c
Ogola Eddy <eddogola01@gmail.com>
2021-03-12 09:18:56 +0300

    exported functions requiring private auth

--- private.go
+++ private.go
@@ -5,10 +5,12 @@ import (
 	"io/ioutil"
 )
 
-// private Client methods
-// Note: Without a Bearer token (i.e. using a Client-ID token) these requests will return a 401 Unauthorized response.
+/*
+	Private Client methods
+	Note: Without a Bearer token (i.e. using a Client-ID token) these requests will return a 401 Unauthorized response.
+*/
 
-// GetUserPrivateProfile gets the user’s private profile
+// GetUserPrivateProfile takes a context and returns the private profile of the authenticated user
 // Note: To access a user’s private data, the user is required to authorize the read_user scope.
 func (c *Client) GetUserPrivateProfile(ctx context.Context) (*User, error) {
 	// check if client is private to do private requests
@@ -33,7 +35,8 @@ func (c *Client) GetUserPrivateProfile(ctx context.Context) (*User, error) {
 	return &usr, nil
 }
 
-// UpdateUserProfile updates the current user’s profile
+// UpdateUserProfile takes a context and a map with the data to update a user. Returns the updated profile.
+// Updates the current user’s profile.
 // https://unsplash.com/documentation#update-the-current-users-profile
 // Note: This action requires the write_user scope. Without it, it will return a 403 Forbidden response.
 func (c *Client) UpdateUserProfile(ctx context.Context, updatedData map[string]string) (*User, error) {
@@ -46,7 +49,6 @@ func (c *Client) UpdateUserProfile(ctx context.Context, updatedData map[string]s
 		return nil, errRequiredScopeAbsent(WriteUserScope)
 	}
 	// make PUT request
-	// response returns the updated profile
 	resp, err := c.putHTTP(ctx, PrivateUserProfileEndpoint, updatedData)
 	if err != nil {
 		return nil, err
@@ -66,10 +68,10 @@ func (c *Client) UpdateUserProfile(ctx context.Context, updatedData map[string]s
 	return &usr, nil
 }
 
-// UpdatePhoto updates a photo on behalf of the logged-in user
-// This requires the `write_photos` scope
+// UpdatePhoto takes in a context, photo id and data with which to update photo. Returns the updated Photo.
+// Updates a photo on behalf of the logged-in user. This requires the `write_photos` scope.
 // https://unsplash.com/documentation#update-a-photo
-func (c *Client) UpdatePhoto(ctx context.Context, ID string, updatedData map[string]string) (*Photo, error) {
+func (c *Client) UpdatePhoto(ctx context.Context, collectionID string, updatedData map[string]string) (*Photo, error) {
 	// check if client is private to do private requests
 	if !isClientPrivate(c) {
 		return nil, errClientNotPrivate
@@ -79,8 +81,7 @@ func (c *Client) UpdatePhoto(ctx context.Context, ID string, updatedData map[str
 		return nil, errRequiredScopeAbsent(WritePhotosScope)
 	}
 	// make PUT request
-	// response returns the updated profile
-	endPoint := AllPhotosEndpoint + ID
+	endPoint := AllPhotosEndpoint + collectionID
 	resp, err := c.putHTTP(ctx, endPoint, updatedData)
 	if err != nil {
 		return nil, err
@@ -100,10 +101,10 @@ func (c *Client) UpdatePhoto(ctx context.Context, ID string, updatedData map[str
 	return &pic, nil
 }
 
-// LikePhoto likes a photo on behalf of the logged-in user
-// This requires the `write_likes` scope
+// LikePhoto takes in a context and photo id, returns abbreviated versions of the picture and user.
+// Likes a photo on behalf of the logged-in user. This requires the `write_likes` scope.
 // https://unsplash.com/documentation#like-a-photo
-func (c *Client) LikePhoto(ctx context.Context, ID string) (*LikeResponse, error) {
+func (c *Client) LikePhoto(ctx context.Context, collectionID string) (*LikeResponse, error) {
 	// check if client is private to do private requests
 	if !isClientPrivate(c) {
 		return nil, errClientNotPrivate
@@ -113,8 +114,7 @@ func (c *Client) LikePhoto(ctx context.Context, ID string) (*LikeResponse, error
 		return nil, errRequiredScopeAbsent(WriteLikesScope)
 	}
 	// make POST request
-	// response returns abbreviated versions of the picture and user
-	endPoint := AllPhotosEndpoint + ID + "/like"
+	endPoint := AllPhotosEndpoint + collectionID + "/like"
 	resp, err := c.postHTTP(ctx, endPoint, nil)
 	if err != nil {
 		return nil, err
@@ -134,16 +134,18 @@ func (c *Client) LikePhoto(ctx context.Context, ID string) (*LikeResponse, error
 	return &lr, nil
 }
 
-// UnlikePhoto removes the logged-in user’s like of a photo.
+// UnlikePhoto takes in a context and photo id. Returns an error if failed
+// deleting like from Photo, nil otherwise.
+// Removes the logged-in user’s like of a photo.
 // https://unsplash.com/documentation#unlike-a-photo
-func (c *Client) UnlikePhoto(ctx context.Context, ID string) error {
+func (c *Client) UnlikePhoto(ctx context.Context, collectionID string) error {
 	// check if client is private to do private requests
 	if !isClientPrivate(c) {
 		return errClientNotPrivate
 	}
 	// make DELETE request
 	// responds with a 204 status code and an empty body
-	endPoint := AllPhotosEndpoint + ID + "/like"
+	endPoint := AllPhotosEndpoint + collectionID + "/like"
 	resp, err := c.deleteHTTP(ctx, endPoint, nil)
 	if err != nil {
 		return err
@@ -155,9 +157,11 @@ func (c *Client) UnlikePhoto(ctx context.Context, ID string) error {
 	return nil
 }
 
-// CreateCollection creates a new collection. This requires the `write_collections` scope.
+// CreateCollection takes in a context and the new collection's data. Returns a pointer to
+// the created collection.
+// Creates a new collection. This requires the `write_collections` scope.
 // https://unsplash.com/documentation#create-a-new-collection
-func (c *Client) CreateCollection(ctx context.Context, ID string, data map[string]string) (*Collection, error) {
+func (c *Client) CreateCollection(ctx context.Context, data map[string]string) (*Collection, error) {
 	// check if client is private to do private requests
 	if !isClientPrivate(c) {
 		return nil, errClientNotPrivate
@@ -187,10 +191,12 @@ func (c *Client) CreateCollection(ctx context.Context, ID string, data map[strin
 	return &collection, nil
 }
 
-// UpdateCollection updates an existing collection belonging to the logged-in user. This requires the `write_collections` scope.
+// UpdateCollection takes in a context, collection's id and data with which to update the collection.
+// Returns a pointer to the updated collection if collection with the provided collection is found.
+// Updates an existing collection belonging to the logged-in user. This requires the `write_collections` scope.
 // https://unsplash.com/documentation#update-an-existing-collection
 // check if client is private to do private requests
-func (c *Client) UpdateCollection(ctx context.Context, ID string, data map[string]string) (*Collection, error) {
+func (c *Client) UpdateCollection(ctx context.Context, collectionID string, data map[string]string) (*Collection, error) {
 	// check if client is private to do private requests
 	if !isClientPrivate(c) {
 		return nil, errClientNotPrivate
@@ -201,7 +207,7 @@ func (c *Client) UpdateCollection(ctx context.Context, ID string, data map[strin
 	}
 	// make PUT request
 	// responds with the updated collection
-	endPoint := CollectionsListEndpoint + ID
+	endPoint := CollectionsListEndpoint + collectionID
 	resp, err := c.putHTTP(ctx, endPoint, data)
 	if err != nil {
 		return nil, err
@@ -221,9 +227,11 @@ func (c *Client) UpdateCollection(ctx context.Context, ID string, data map[strin
 	return &collection, nil
 }
 
-// DeleteCollection deletes a collection belonging to the logged-in user. This requires the `write_collections` scope.
+// DeleteCollection takes in a context and collection's id. Returns an error if delete has failed,
+// nil otherwise.
+// Deletes a collection belonging to the logged-in user. This requires the `write_collections` scope.
 // https://unsplash.com/documentation#delete-a-collection
-func (c *Client) DeleteCollection(ctx context.Context, ID string) error {
+func (c *Client) DeleteCollection(ctx context.Context, collectionID string) error {
 	// check if client is private to do private requests
 	if !isClientPrivate(c) {
 		return errClientNotPrivate
@@ -234,7 +242,7 @@ func (c *Client) DeleteCollection(ctx context.Context, ID string) error {
 	}
 	// make DELETE request
 	// responds with a 204 status code and an empty body
-	endPoint := CollectionsListEndpoint + ID
+	endPoint := CollectionsListEndpoint + collectionID
 	resp, err := c.deleteHTTP(ctx, endPoint, nil)
 	if err != nil {
 		return err
@@ -247,9 +255,11 @@ func (c *Client) DeleteCollection(ctx context.Context, ID string) error {
 	return nil
 }
 
-// AddPhotoToCollection adds a photo to one of the logged-in user’s collections. Requires the `write_collections` scope.
+// AddPhotoToCollection takes in a context, collection id, and data with photo details. `data` must have `photo_id`.
+// Returns abbreviated versions of the added picture and user.
+// Adds a photo to one of the logged-in user’s collections. Requires the `write_collections` scope.
 // https://unsplash.com/documentation#add-a-photo-to-a-collection
-func (c *Client) AddPhotoToCollection(ctx context.Context, ID string, data map[string]string) (*CollectionActionResponse, error) {
+func (c *Client) AddPhotoToCollection(ctx context.Context, collectionID string, data map[string]string) (*CollectionActionResponse, error) {
 	// check if client is private to do private requests
 	if !isClientPrivate(c) {
 		return nil, errClientNotPrivate
@@ -259,8 +269,7 @@ func (c *Client) AddPhotoToCollection(ctx context.Context, ID string, data map[s
 		return nil, errRequiredScopeAbsent(WriteCollectionsScope)
 	}
 	// make POST request
-	// response returns abbreviated versions of the picture and user
-	endPoint := CollectionsListEndpoint + ID + "/add"
+	endPoint := CollectionsListEndpoint + collectionID + "/add"
 	resp, err := c.postHTTP(ctx, endPoint, data)
 	if err != nil {
 		return nil, err
@@ -280,9 +289,11 @@ func (c *Client) AddPhotoToCollection(ctx context.Context, ID string, data map[s
 	return &car, nil
 }
 
-// RemovePhotoFromCollection removes a photo from one of the logged-in user’s collections. Requires the `write_collections` scope.
+// RemovePhotoFromCollection takes in a context, collection id, and data with photo details. `data` must have `photo_id`.
+// Returns abbreviated versions of the deleted picture and user.
+// Removes a photo from one of the logged-in user’s collections. Requires the `write_collections` scope.
 // https://unsplash.com/documentation#remove-a-photo-from-a-collection
-func (c *Client) RemovePhotoFromCollection(ctx context.Context, ID string, data map[string]string) (*CollectionActionResponse, error) {
+func (c *Client) RemovePhotoFromCollection(ctx context.Context, collectionID string, data map[string]string) (*CollectionActionResponse, error) {
 	// check if client is private to do private requests
 	if !isClientPrivate(c) {
 		return nil, errClientNotPrivate
@@ -293,7 +304,7 @@ func (c *Client) RemovePhotoFromCollection(ctx context.Context, ID string, data 
 	}
 	// make DELETE request
 	// responds with a 204 status code and an empty body
-	endPoint := CollectionsListEndpoint + ID + "/remove"
+	endPoint := CollectionsListEndpoint + collectionID + "/remove"
 	resp, err := c.deleteHTTP(ctx, endPoint, data)
 	if err != nil {
 		return nil, err
