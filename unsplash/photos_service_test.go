7ee96abf103c0c2b245741639b87d5ff09dc64bf
Ogola Eddy <eddogola01@gmail.com>
2021-03-17 12:37:13 +0300

    Add mock PhotosService interface and mock Unsplash object
    Implement the PhotosServiceClient interface using a mock struct
    passed to Unsplash.Photos to mock the whole service

--- unsplash/photos_service_test.go
+++ unsplash/photos_service_test.go
@@ -2,17 +2,71 @@ package unsplash
 
 import (
 	"context"
-	"os"
 	"testing"
 
 	"github.com/eddogola/unsplash-go/unsplash/client"
 )
 
+var pics = []client.Photo{
+	{
+		ID: "whackID",
+		Width: 1308,
+		Height: 768,
+		Downloads: 400,
+		Likes: 799,
+	},
+	{
+		ID: "noID",
+		Width: 450,
+		Height: 600,
+	},
+}
+
+var pic = client.Photo{
+	ID: "whackID",
+	Width: 1308,
+	Height: 768,
+	Downloads: 400,
+	Likes: 799,
+}
+
+type mockPhotoServiceClient struct{}
+
+func (m *mockPhotoServiceClient) GetPhotoList(ctx context.Context, queryParams client.QueryParams) ([]client.Photo, error) {
+	return pics, nil
+}
+func (m *mockPhotoServiceClient) GetPhoto(ctx context.Context, photoID string) (*client.Photo, error) {
+	return &pic, nil
+}
+func (m *mockPhotoServiceClient) GetRandomPhoto(ctx context.Context, queryParams client.QueryParams) (interface{}, error) {
+	if _, ok := queryParams["count"]; ok {
+		return pics, nil
+	}
+	return &pic, nil
+}
+func (m *mockPhotoServiceClient) GetPhotoStats(ctx context.Context,photoID  string, queryParams client.QueryParams) (*client.PhotoStats, error) {
+	return &client.PhotoStats{ID: "ladida"}, nil
+}
+func (m *mockPhotoServiceClient) SearchPhotos(ctx context.Context, queryParams client.QueryParams) (*client.PhotoSearchResult, error){
+	return &client.PhotoSearchResult{Results: pics}, nil
+}
+func (m *mockPhotoServiceClient) UpdatePhoto(ctx context.Context, photoID string, updatedData map[string]string) (*client.Photo, error){
+	return &pic, nil
+}
+func (m *mockPhotoServiceClient) LikePhoto(ctx context.Context, photoID string) (*client.LikeResponse, error){
+	return &client.LikeResponse{Photo: pic}, nil
+}
+func (m *mockPhotoServiceClient) UnlikePhoto(ctx context.Context, photoID string) (error){
+	return nil
+}
+
 func TestPhotosService(t *testing.T) {
+	mockPhotoService := &PhotosService{client: &mockPhotoServiceClient{}}
+	unsplash := &Unsplash{
+		Photos: mockPhotoService,
+	}
 
 	t.Run("random photo when count not passed", func(t *testing.T) {
-		cl := client.New(os.Getenv("CLIENT_ID"), nil, client.NewConfig())
-		unsplash := New(cl)
 		res, err := unsplash.Photos.GetRandom(context.Background(), nil)
 		randomPhoto := res.(*client.Photo)
 		checkErrorIsNil(t, err)
@@ -20,9 +74,6 @@ func TestPhotosService(t *testing.T) {
 	})
 
 	t.Run("random photo when count passed", func(t *testing.T) {
-		cl := client.New(os.Getenv("CLIENT_ID"), nil, client.NewConfig())
-		unsplash := New(cl)
-
 		res, err := unsplash.Photos.GetRandom(context.Background(), client.QueryParams{"count": "1"})
 		randomPhotos := res.([]client.Photo)
 		checkErrorIsNil(t, err)
