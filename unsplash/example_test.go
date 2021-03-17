b646e8c555899c7c95441a07e438f32559d82ccf
Ogola Eddy <eddogola01@gmail.com>
2021-03-17 12:39:16 +0300

    Add example to show how to get random photo

---
+++ unsplash/example_test.go
@@ -0,0 +1,23 @@
+package unsplash
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/eddogola/unsplash-go/unsplash/client"
+)
+
+func ExamplePhotosService_GetRandom() {
+	cl := client.New(os.Getenv("CLIENT_ID"), nil, client.NewConfig())
+	unsplash := New(cl)
+	res, err := unsplash.Photos.GetRandom(context.Background(), nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	
+	randomPhoto := res.(*client.Photo)
+	randomPhoto.ID = "losemycool"
+	fmt.Println(randomPhoto.URLs)
+	// losemycool
+}
