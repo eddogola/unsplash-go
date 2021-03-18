763ded837b9dc3c4beac14aaa46218137d2a7b26
Ogola Eddy <eddogola01@gmail.com>
2021-03-18 13:03:29 +0300

    Mock TopicsService
    Implement fake methods on a mock TopicsServiceClient to be used in
    tests

--- unsplash/topics_service_test.go
+++ unsplash/topics_service_test.go
@@ -1,1 +1,51 @@
 package unsplash
+
+import (
+	"context"
+	"testing"
+
+	"github.com/eddogola/unsplash-go/unsplash/client"
+)
+
+var topics = []client.Topic{
+	{
+		ID: "cole",
+		Title: "king Cole",
+		CoverPhoto: pic,
+	},
+	{
+		ID: "kendrick",
+		Title: "king Kunta",
+		CoverPhoto: pic,
+	},
+}
+
+var topic = topics[0]
+
+type mockTopicsServiceClient struct{}
+
+func (m *mockTopicsServiceClient) GetTopicsList(ctx context.Context, queryParams client.QueryParams) ([]client.Topic, error) {
+	return topics, nil
+}
+
+func (m *mockTopicsServiceClient) GetTopic(ctx context.Context, topicIDOrSlug string) (*client.Topic, error) {
+	topic.ID = topicIDOrSlug
+	topic.Slug = topicIDOrSlug
+	return &topic, nil
+}
+
+func (m *mockTopicsServiceClient) GetTopicPhotos(ctx context.Context, topicIDOrSlug string, queryParams client.QueryParams) ([]client.Photo, error) {
+	return pics, nil
+}
+
+func TestTopicsService(t *testing.T) {
+	mockUnsplash := &Unsplash{
+		Topics: &TopicsService{client: &mockTopicsServiceClient{}},
+	}
+
+	t.Run("all topics", func(t *testing.T) {
+		res, err := mockUnsplash.Topics.All(context.Background(), nil)
+		checkErrorIsNil(t, err)
+		checkRsNotNil(t, res)
+	})
+}
