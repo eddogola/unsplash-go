47afad26427ef3fe576c88be7c341bdff41b58ca
Ogola Eddy <eddogola01@gmail.com>
2021-03-19 14:59:11 +0300

    Fix errors captured by golangci-lint
    Check error in parseJSON
    Add comments for exported errors

--- unsplash/client/errors.go
+++ unsplash/client/errors.go
@@ -14,12 +14,20 @@ type Errors struct {
 }
 
 var (
+	// ErrCodeQueryParamNotFound is raised when code query parameter is not in the url used for
+	// oauth token access.
 	ErrCodeQueryParamNotFound = errors.New("`code` query parameter not found in the request URL")
+	// ErrClientNotPrivate is raised when the client usedd with the Unsplash object is not authentocated
+	// for private functions.
 	ErrClientNotPrivate       = errors.New("client not private but used for functions that require private authentication")
+	// ErrAuthCodeEmpty is raised when the authorization code is empty.
 	ErrAuthCodeEmpty          = errors.New("Auth code provided is empty")
 )
 
+// ErrQueryNotInURL is raised when a search query parameter is not part of the url.
 type ErrQueryNotInURL string
+// ErrRequiredScopeAbsent is raised on trying to access a private action
+// when the required scope is not provided or allowed from the authenticated user's endd.
 type ErrRequiredScopeAbsent string
 
 func (e ErrQueryNotInURL) Error() string {
@@ -30,6 +38,8 @@ func (e ErrRequiredScopeAbsent) Error() string {
 	return "required scope `%v` not in client auth scopes"
 }
 
+// ErrStatusCode defines a http status code error
+// with the status code and tthe reasons for the error
 type ErrStatusCode struct {
 	statusCode int
 	reasons    []string
@@ -43,7 +53,8 @@ func getErrReasons(resp *http.Response) []string {
 	data, _ := ioutil.ReadAll(resp.Body)
 
 	var errs Errors
-	parseJSON(data, &errs)
+	err := parseJSON(data, &errs)
 
+	errs.ErrorList = append(errs.ErrorList, err.Error())
 	return errs.ErrorList
 }
