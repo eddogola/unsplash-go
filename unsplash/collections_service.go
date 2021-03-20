be25804a88c0334a51266662d0c9a7e39babb0d5
Ogola Eddy <eddogola01@gmail.com>
2021-03-20 13:27:24 +0300

    Remove context from function parameters
    Provide default context.Background in function body.

--- unsplash/collections_service.go
+++ unsplash/collections_service.go
@@ -27,27 +27,32 @@ type CollectionsService struct {
 }
 
 // All returns a paginated list of all Collections on Unsplash
-func (cs *CollectionsService) All(ctx context.Context, queryParams client.QueryParams) ([]client.Collection, error) {
+func (cs *CollectionsService) All(queryParams client.QueryParams) ([]client.Collection, error) {
+	ctx := context.Background()
 	return cs.client.GetCollectionsList(ctx, queryParams)
 }
 
 // Get returns a specific Collection, given its id
-func (cs *CollectionsService) Get(ctx context.Context, collectionID int) (*client.Collection, error) {
+func (cs *CollectionsService) Get(collectionID int) (*client.Collection, error) {
+	ctx := context.Background()
 	return cs.client.GetCollection(ctx, collectionID)
 }
 
 // Photos returns a paginated list of Photos under the given collection
-func (cs *CollectionsService) Photos(ctx context.Context, collectionID int, queryParams client.QueryParams) ([]client.Photo, error) {
+func (cs *CollectionsService) Photos(collectionID int, queryParams client.QueryParams) ([]client.Photo, error) {
+	ctx := context.Background()
 	return cs.client.GetCollectionPhotos(ctx, collectionID, queryParams)
 }
 
 // Related returns a paginated list of collections related to the given collection
-func (cs *CollectionsService) Related(ctx context.Context, collectionID int) ([]client.Collection, error) {
+func (cs *CollectionsService) Related(collectionID int) ([]client.Collection, error) {
+	ctx := context.Background()
 	return cs.client.GetRelatedCollections(ctx, collectionID)
 }
 
 // Search takes in a search query under the given query parameters to return a list of Collection search results
-func (cs *CollectionsService) Search(ctx context.Context, searchQuery string, queryParams client.QueryParams) (*client.CollectionSearchResult, error) {
+func (cs *CollectionsService) Search(searchQuery string, queryParams client.QueryParams) (*client.CollectionSearchResult, error) {
+	ctx := context.Background()
 	if queryParams == nil || queryParams["query"] == "" {
 		queryParams = make(client.QueryParams)
 		queryParams["query"] = searchQuery
@@ -60,27 +65,32 @@ func (cs *CollectionsService) Search(ctx context.Context, searchQuery string, qu
 // methods requiring private authentication
 
 // Create creates a new collection using the data provided in the map, returning it if the creation process is successful
-func (cs *CollectionsService) Create(ctx context.Context, data map[string]string) (*client.Collection, error) {
+func (cs *CollectionsService) Create(data map[string]string) (*client.Collection, error) {
+	ctx := context.Background()
 	return cs.client.CreateCollection(ctx, data)
 }
 
 // Update uses the data provided in the map to update the given collection
 // returning the updated collection
-func (cs *CollectionsService) Update(ctx context.Context, collectionID string, data map[string]string) (*client.Collection, error) {
+func (cs *CollectionsService) Update(collectionID string, data map[string]string) (*client.Collection, error) {
+	ctx := context.Background()
 	return cs.client.UpdateCollection(ctx, collectionID, data)
 }
 
 // Delete removes the given collection
-func (cs *CollectionsService) Delete(ctx context.Context, collectionID string) error {
+func (cs *CollectionsService) Delete(collectionID string) error {
+	ctx := context.Background()
 	return cs.client.DeleteCollection(ctx, collectionID)
 }
 
 // AddPhoto takes in a `photo_id` in the data map, to add the Photo to the given collection
-func (cs *CollectionsService) AddPhoto(ctx context.Context, collectionID string, data map[string]string) (*client.CollectionActionResponse, error) {
+func (cs *CollectionsService) AddPhoto(collectionID string, data map[string]string) (*client.CollectionActionResponse, error) {
+	ctx := context.Background()
 	return cs.client.AddPhotoToCollection(ctx, collectionID, data)
 }
 
 // RemovePhoto takes in a `photo_id` in the data map, to remove the Photo from the given collection
-func (cs *CollectionsService) RemovePhoto(ctx context.Context, collectionID string, data map[string]string) (*client.CollectionActionResponse, error) {
+func (cs *CollectionsService) RemovePhoto(collectionID string, data map[string]string) (*client.CollectionActionResponse, error) {
+	ctx := context.Background()
 	return cs.client.RemovePhotoFromCollection(ctx, collectionID, data)
 }
