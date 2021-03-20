034027a9c835e93bdaeb34b06514c9f264d8da12
Ogola Eddy <eddogola01@gmail.com>
2021-03-20 13:26:37 +0300

    Remove context from function parameters
    Provide default context.Background in function body.

--- unsplash/photos_service.go
+++ unsplash/photos_service.go
@@ -26,28 +26,33 @@ type PhotosService struct {
 }
 
 // All returns a paginated list of all the Photos on Unsplash
-func (ps *PhotosService) All(ctx context.Context, queryParams client.QueryParams) ([]client.Photo, error) {
+func (ps *PhotosService) All(queryParams client.QueryParams) ([]client.Photo, error) {
+	ctx := context.Background()
 	return ps.client.GetPhotoList(ctx, queryParams)
 }
 
 // Get returns a single Photo, requested using the photo's ID
-func (ps *PhotosService) Get(ctx context.Context, photoID string) (*client.Photo, error) {
+func (ps *PhotosService) Get(photoID string) (*client.Photo, error) {
+	ctx := context.Background()
 	return ps.client.GetPhoto(ctx, photoID)
 }
 
 // Random returns a random Photo.
 // Returns a paginated list of Photos if `count` query parameter is provided in the query parameters.
-func (ps *PhotosService) Random(ctx context.Context, queryParams client.QueryParams) (interface{}, error) {
+func (ps *PhotosService) Random(queryParams client.QueryParams) (interface{}, error) {
+	ctx := context.Background()
 	return ps.client.GetRandomPhoto(ctx, queryParams)
 }
 
 // Stats returns the requested Photo's Stats
-func (ps *PhotosService) Stats(ctx context.Context, photoID string, queryParams client.QueryParams) (*client.PhotoStats, error) {
+func (ps *PhotosService) Stats(photoID string, queryParams client.QueryParams) (*client.PhotoStats, error) {
+	ctx := context.Background()
 	return ps.client.GetPhotoStats(ctx, photoID, queryParams)
 }
 
 // Search takes in a search query in the query parameters and returns a list of Photo search results
-func (ps *PhotosService) Search(ctx context.Context, searchQuery string, queryParams client.QueryParams) (*client.PhotoSearchResult, error) {
+func (ps *PhotosService) Search(searchQuery string, queryParams client.QueryParams) (*client.PhotoSearchResult, error) {
+	ctx := context.Background()
 	if queryParams == nil {
 		queryParams = make(client.QueryParams)
 		queryParams["query"] = searchQuery
@@ -60,16 +65,19 @@ func (ps *PhotosService) Search(ctx context.Context, searchQuery string, queryPa
 // methods requiring private authentication
 
 // Update uses the data provided to update info on the requested Photo
-func (ps *PhotosService) Update(ctx context.Context, photoID string, updatedData map[string]string) (*client.Photo, error) {
+func (ps *PhotosService) Update(photoID string, updatedData map[string]string) (*client.Photo, error) {
+	ctx := context.Background()
 	return ps.client.UpdatePhoto(ctx, photoID, updatedData)
 }
 
 // Like adds a like on the photo whose photo ID is provided on behalf of the authenticated user
-func (ps *PhotosService) Like(ctx context.Context, photoID string) (*client.LikeResponse, error) {
+func (ps *PhotosService) Like(photoID string) (*client.LikeResponse, error) {
+	ctx := context.Background()
 	return ps.client.LikePhoto(ctx, photoID)
 }
 
 // Unlike removes a like on the photo whose photo ID is provided on behalf of the authenticated user
-func (ps *PhotosService) Unlike(ctx context.Context, photoID string) error {
+func (ps *PhotosService) Unlike(photoID string) error {
+	ctx := context.Background()
 	return ps.client.UnlikePhoto(ctx, photoID)
 }
