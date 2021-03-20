078c0e5b9ece9e01e4dcc4dde2a3698c11b3d435
Ogola Eddy <eddogola01@gmail.com>
2021-03-20 17:05:23 +0300

    Format with gofmt

--- unsplash/client/collections.go
+++ unsplash/client/collections.go
@@ -7,7 +7,7 @@ import (
 
 // Collection defines fields in a collection resource
 type Collection struct {
-	ID              string    `json:"id"`
+	ID              string `json:"id"`
 	Title           string `json:"title"`
 	Description     string `json:"description"`
 	PublishedAt     string `json:"published_at"`
