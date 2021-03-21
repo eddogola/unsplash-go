34515a334388be50e2afa618de2f48e092e4335e
Ogola Eddy <eddogola01@gmail.com>
2021-03-21 17:47:33 +0300

    Add create collection example

--- unsplash/examples/collections/main.go
+++ unsplash/examples/collections/main.go
@@ -10,36 +10,58 @@ import (
 
 func main() {
 	// Initialize unsplash for public actions
-	unsplash := unsplash.New(client.New(
+	publicUnsplash := unsplash.New(client.New(
 		os.Getenv("CLIENT_ID"),
 		nil, // when nil is passed, http.DefaultClient is used
 		client.NewConfig(),
 	))
 
 	// all collections
-	collections, err := unsplash.Collections.All(nil)
+	collections, err := publicUnsplash.Collections.All(nil)
 	checkErr(err)
 	fmt.Println(collections[0].Links.HTML)
 
 	// get collection
-	collection, err := unsplash.Collections.Get(collections[0].ID)
+	collection, err := publicUnsplash.Collections.Get(collections[0].ID)
 	checkErr(err)
 	fmt.Println(collection.Title)
 
 	// collection photos
-	photos, err := unsplash.Collections.Photos(collections[0].ID, nil)
+	photos, err := publicUnsplash.Collections.Photos(collections[0].ID, nil)
 	checkErr(err)
 	fmt.Println(photos[0].Links.HTML)
 
 	// related collections
-	related, err := unsplash.Collections.Related(collections[0].ID)
+	related, err := publicUnsplash.Collections.Related(collections[0].ID)
 	checkErr(err)
 	fmt.Println(related[0].Title)
 
 	// search collections
-	searchResults, err := unsplash.Collections.Search("code", nil)
+	searchResults, err := publicUnsplash.Collections.Search("code", nil)
 	checkErr(err)
 	fmt.Println(searchResults.Results[0].Title)
+
+	/**Setup private client for actions requiring private authorization**/
+	privClient, err := client.NewPrivateAuthClient(
+		os.Getenv("CLIENT_ID"),
+		os.Getenv("CLIENT_SECRET"),
+		os.Getenv("REDIRECT_URI"),
+		client.NewAuthScopes("write_likes"),
+		client.NewConfig(),
+	)
+	privateUnsplash := unsplash.New(privClient)
+	/*********************************************************************/
+
+	// create collection
+	collection, err = privateUnsplash.Collections.Create(map[string]string{
+		"title": "unsplash-go test",
+		"description": "will run travis tests then disappear",
+	})
+	checkErr(err)
+	if collection.Title != "unsplash-go test" {
+		fmt.Printf("expected %v but got %v", "unsplash-go test", collection.Title)
+		os.Exit(1)
+	}
 }
 
 func checkErr(err error) {
